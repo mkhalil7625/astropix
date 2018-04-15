0d2a63ed5511ccd7554bcb40dcd48c4caee89176
mkhalil7625 <mkhalil7625@gmail.com>
2018-04-15 15:40:08 -0500

    4/15/18

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs=require('hbs');
+var session=require('express-session');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
+//connect the second router
+var favorites=require('./routes/favorites');
 
 var app = express();
 
@@ -21,10 +24,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret:'top secret',
+    saveUninitialized: true,
+    resave:true
+}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+//app.use('/users', usersRouter);
+app.use('/favorites', favorites);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/favorites.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router=express.Router();
+//get favorite page which will be the home page for this router
+router.get('/',function (req, res, next) {
+    res.render('favorites',{favorites:req.session.favorites});
+});
+//post to add new favorite to user's favorits
+router.post('/add', function (req, res, next) {
+    //if a favorites array doesnot exist create it
+    if(!req.session.favorites){
+        req.session.favorites=[];
+        }
+        //filter favorites array for image with that date
+    var favorite_on_date=re1.session.favorites.filter(function (fav) {
+        return fav.date==req.body.date
+    });
+    //if no favorites with that date, then add to the array
+    if (favorite_on_date.length==0){
+        req.session.favorites.push(req.body);
+    }
+    //redierect to the favorites page
+    res.redirect('/favorites')
+});
+module.exports=router;
